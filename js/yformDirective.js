a5cf991bbe2d8b36f2ec1f9f71fcb1f5c83d7f33
Johannes Harms <johannes.harms@inso.tuwien.ac.at>
2013-03-05 15:45:43 +0100

    Rekursives Form-Element-Rendering funktioniert!

--- js/yformDirective.js
+++ js/yformDirective.js
@@ -1,32 +1,22 @@
-app.directive('yform', function () {
+app.directive('yform', function ($compile) {
   return {
     template: '<div></div>',
     replace: true,
     transclude: true,
-    restrict: 'E',
-    scope: {formData: '=formData', formSchema: '=formSchema'},
-    controller: function ($scope, $element, $transclude, $compile) {
-      var child = angular.element('<div widget-type-' + $scope.formSchema.widgetType + '></div>');
-      $element.append(child);
-      $compile(child, $scope);
-      // $transclude(function (clone) {
-      //   clone.attr('widget-type-' + $scope.formSchema.widgetType, '');
-      // })
+    scope: {
+      formData: '=formData', 
+      formSchema: '=formSchema'
     },
-    compile: function compile(tElement, tAttrs, transclude  /* = linker function */ ) {
-      console.log('compile');
-      // console.log(transclude);
-      // transclude({}, function (clone) {
-      //    console.log('transclude');
-      //    clone.attr('widget-type-' + $scope.formSchema.widgetType, '');
-      // });
-      return {
-        pre: function preLink(scope, iElement, iAttrs, controller) { 
-          console.log('pre');
-          //iElement.append('<div widget-type-' + scope.formSchema.widgetType + '></div>');
-        },
-        post: function postLink(scope, iElement, iAttrs, controller) { }
-      }
+    link: function(scope, iElement, iAttrs) {
+      console.log('linking: ', scope, iElement, iAttrs);
+      
+      // render myself:
+      var widget = angular.element('<div widget-type-' + scope.formSchema.widgetType + '>{{formSchema.widgetType}}</div>');
+      iElement.append( $compile(widget)(scope) );
+      
+      // render all children:
+      var children = angular.element('<div class="children" ng-repeat="formSchema in formSchema.children">{{formSchema.widgetType}}: <div yform form-schema="formSchema" form-data="formData[formSchema.id]"></div></div>');
+      iElement.append( $compile(children)(scope) );
     }
   };
 });
